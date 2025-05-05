33057aa44778ab18a777a8b65096b65d28b05a3e
Raptor <francyoppureno@gmail.com>
2025-05-05 18:09:15 +0200

    General home layout

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -10,12 +10,12 @@ interface Props {
 
 const Navbar = ({ session }: Props) => {
   return (
-    <nav className='px-4 h-14 flex items-center justify-between'>
+    <nav className='fixed w-full bg-background border border-x-0 border-t-0 border-b-border px-4 py-2 md:px-8 md:py-4 flex items-center justify-between'>
       <ModeToggle />
       <Link
         href={session ? "/api/auth/signout" : "/api/auth/signin"}
         className={clsx(
-          'rounded-md px-10 py-3 font-semibold no-underline transition',
+          'rounded-md px-10 py-2 font-semibold no-underline transition',
           session ? 'bg-white/10 hover:bg-white/20' : 'bg-blurple hover:bg-blurple/70'
         )}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,36 @@
 import { auth } from "@/server/auth";
 import Navbar from "./components/Navbar/Navbar";
+import MapCard from "./components/MapCard/MapCard";
 
 export default async function Home() {
   const session = await auth();
 
   return (
-    <div>
+    <>
       <Navbar session={session} />
-    </div>
+      <main className="pt-18 md:pt-22 pb-8 px-4 md:px-8 gap-4 grid grid-cols-1 md:grid-cols-3 overflow-scroll">
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+        <MapCard></MapCard>
+      </main>
+    </>
   )
 }
